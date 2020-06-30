f6995d323c84e5d19a2ddc47140f406795217e10
Salman Zafar <maanizfar@gmail.com>
2020-06-30 17:39:38 +0500

    add products endpoint

--- products/models.py
+++ products/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Product(models.Model):
 
     gender_choices = [
-        ('M', 'Men'),
-        ('F', 'Women'),
-        ('K', 'Kids')
+        ('MEN', 'Men'),
+        ('WOMEN', 'Women'),
+        ('KIDS', 'Kids')
     ]
 
     category_choices = [
@@ -23,9 +23,9 @@ class Product(models.Model):
 
     name = models.CharField(max_length=200)
     brand = models.CharField(max_length=20, choices=brands, default='NIKE')
-    main_category = models.CharField(
-        max_length=1, choices=gender_choices,)
-    sub_category = models.CharField(
+    gender = models.CharField(
+        max_length=5, choices=gender_choices,)
+    category = models.CharField(
         max_length=10, choices=category_choices,)
     price = models.PositiveIntegerField()
     imageURL = models.URLField()
