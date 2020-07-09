225e56dee991ea74aa48b8b9f418764677e0dd97
Salman Zafar <maanizfar@gmail.com>
2020-07-09 23:28:26 +0500

    add slug field to serializer

--- products/serializers.py
+++ products/serializers.py
@@ -3,7 +3,12 @@ from .models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    slug = serializers.SerializerMethodField('get_slug')
+
+    def get_slug(self, instance):
+        return instance.name.lower().replace(' ', '-')
+
     class Meta:
         model = Product
         fields = ['id', 'name', 'brand', 'gender', 'category',
-                  'price', 'is_in_inventory', 'items_left', 'imageURL']
+                  'price', 'is_in_inventory', 'items_left', 'imageURL', 'slug']
